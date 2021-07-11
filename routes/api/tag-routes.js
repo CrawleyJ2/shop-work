2d6d9fb92dc6e029d482143b34e14fb05a635797
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-07-11 17:53:29 -0400

    added sequelize sync to server and removed model from tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Model } = require('sequelize/types');
 const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
